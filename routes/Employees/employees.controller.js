ce153856d380126bd17cdf909a0f413c09412c51
Velomad <sagaryc111@gmail.com>
2021-04-15 04:00:37 +0530

    Fixes

--- routes/Employees/employees.controller.js
+++ routes/Employees/employees.controller.js
@@ -1,70 +1,89 @@
-require('dotenv').config();
-const { create, updateRefreshToken, verifyEmployeesByEmail } = require('./employees.service');
-const { genSaltSync, hashSync, compareSync } = require('bcrypt');
+require("dotenv").config();
+const {
+  create,
+  updateRefreshToken,
+  verifyEmployeesByEmail,
+} = require("./employees.service");
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-    createEmployee: (req, res) => {
-        var body = req.body.data;
-        const salt = genSaltSync(10);
-        body.employeepassword = hashSync(body.employeepassword, salt);
-        create(body, (err, results) => {
-            if (err) {
-                return res.status(500).json({
-                    success: 0,
-                    error: err,
-                    message: 'DataBase Connection Error'
-                });
-            }
-            return res.status(200).json({
-                message: 'success',
-                data: results
+  createEmployee: (req, res) => {
+    var body = req.body.data;
+    const salt = genSaltSync(10);
+    body.employeepassword = hashSync(body.employeepassword, salt);
+    create(body, (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          success: 0,
+          error: err,
+          message: "DataBase Connection Error",
+        });
+      }
+      return res.status(200).json({
+        message: "success",
+        data: results,
+      });
+    });
+  },
+  verifyEmployeeByEmail: (req, res) => {
+    const body = req.body.user;
+    console.log("body", body.email);
+    let accessToken = jwt.sign(body, "access", { expiresIn: "1h" });
+    let refreshToken = jwt.sign(body, "refresh", { expiresIn: "7d" });
+    verifyEmployeesByEmail(body.email, (err, results) => {
+      if (err) {
+        console.log("Error Login");
+      }
+      if (!results) {
+        return res
+          .status(200)
+          .json({
+            message: "unsuccess",
+            accessToken: "USER_NOT_AUTHENTICATED",
+            refreshToken: "USER_NOT_AUTHENTICATED",
+          });
+      }
+      const result = compareSync(body.password, results.emp_password);
+      console.log("result", result);
+      if (result) {
+        let data = {
+          token: refreshToken,
+          email: body.email,
+        };
+        updateRefreshToken(data, (tokenerr, tokenresults) => {
+          if (tokenerr) {
+            return res.status(201).json({
+              message: "unsuccess",
+              data: "Error updating token.",
             });
+          }
+          if (!tokenresults) {
+            return res.status(201).json({
+              message: "unsuccess",
+              data: tokenerr,
+            });
+          }
+          if (tokenresults) {
+            return res.status(201).json({
+              email: results.email,
+              fullname: results.firstname + " " + results.lastname,
+              authrole: results.loggedinrole,
+              message: "success",
+              accessToken,
+              refreshToken,
+            });
+          }
         });
-    },
-    verifyEmployeeByEmail: (req, res) => {
-        const body = req.body.user;
-        console.log('body', body.email);
-        let accessToken = jwt.sign(body, "access", { expiresIn: "1h" });
-        let refreshToken = jwt.sign(body, "refresh", { expiresIn: "7d" });
-        verifyEmployeesByEmail(body.email, (err, results) => {
-            if (err) {
-                console.log("Error Login");
-            }
-            if (!results) {
-                return res.status(200).json({ 'message': "unsuccess", 'accessToken': 'USER_NOT_AUTHENTICATED', 'refreshToken': 'USER_NOT_AUTHENTICATED' });
-            }
-            const result = compareSync(body.password, results.emp_password);
-            console.log('result', result);
-            if (result) {
-                let data = {
-                    'token': refreshToken,
-                    'email': body.email
-                }
-                updateRefreshToken(data, (tokenerr, tokenresults) => {
-                    if (tokenerr) {
-                        return res.status(201).json({
-                            message: 'unsuccess',
-                            data: 'Error updating token.'
-                        });
-                    }
-                    if (!tokenresults) {
-                        return res.status(201).json({
-                            message: 'unsuccess',
-                            data: tokenerr
-                        });
-                    }
-                    if (tokenresults) {
-                        return res.status(201).json({
-                            message: 'success',
-                            accessToken,
-                            refreshToken
-                        });
-                    }
-                })
-            } else {
-                return res.status(200).json({ message: "unsuccess", accessToken: 'USER_NOT_AUTHENTICATED', refreshToken: 'USER_NOT_AUTHENTICATED' });
-            }
-        });
-    }
-}
+      } else {
+        return res
+          .status(200)
+          .json({
+            message: "unsuccess",
+            accessToken: "USER_NOT_AUTHENTICATED",
+            refreshToken: "USER_NOT_AUTHENTICATED",
+          });
+      }
+    });
+  },
+};
--- routes/Employees/employees.service.js
+++ routes/Employees/employees.service.js
@@ -2,7 +2,7 @@ const pool = require('../../db/db.js');
 
 module.exports = {
     create: (data, callBack) => {
-        let sql = `call sp_createEmployee('','${data.employeeid}','${data.firstname}','${data.lastname}','${data.employeepassword}','${data.phonenumber}','${data.email}','${data.gender}','${data.dob}','${data.empaddress}','${data.empcity}','${data.empstate}','${data.empzipcode}','${data.empcountry}','${data.employeeemail}','${data.employeeid}','${'0'}','${'0'}','${'0'}','${'0'}','${'0'}')`;
+        let sql = `call sp_createEmployee('','${data.employeeid}','${data.firstname}','${data.lastname}','${data.employeepassword}','${data.phonenumber}','${data.email}','${data.gender}','${data.dob}','${data.empaddress}','${data.empcity}','${data.empstate}','${data.empzipcode}','${data.empcountry}','${data.employeerole}','${data.employeeemail}','${data.employeeid}','${'0'}','${'0'}','${'0'}','${'0'}')`;
         pool.query(sql, true, (err, results, fields) => {
             if (err) {
                 return callBack(err)
